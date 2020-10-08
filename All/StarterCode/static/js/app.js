f6c07ec3afc140bac50e578b06fddd15a88887dd
Nicole Zillmer <nicolezillmer@gmail.com>
2020-10-08 13:21:21 -0700

    It WORKSgit add .!

--- All/StarterCode/static/js/app.js
+++ All/StarterCode/static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+//var sightings = data;
+
+var tbody = d3.select("tbody");
+
+data.forEach((ufoSight) =>{
+
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSight).forEach(([key, value]) => {
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var form = d3.select("#form");
+
+var button = d3.select("#filter-btn");
+
+button.on("click", runEnter);
+
+form.on("submit", runEnter);
+
+
+
+function runEnter(){
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filteredData = data.filter(sighting => sighting.datetime == inputValue);
+
+    console.log(filteredData)
+    var sightings = filteredData;
+
+    var tbody = d3.select("tbody");
+
+    filteredData.forEach((ufoSight) =>{
+
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSight).forEach(([key, value]) => {
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+};
